f86d983c0f21f78bf0ea70d45fa288b012a88084
Zeal <xujinzheng@gmail.com>
2015-07-30 17:25:11 +0800

    add isNil templ func and update some logic of template

--- renderer_tmpl_default.go
+++ renderer_tmpl_default.go
@@ -9,16 +9,16 @@ func defaultAPITemplate() (name, tmpl string) {
 	{{if ne .API.Response.ErrorNamespace ""}}"error_namespace":"{{.API.Response.ErrorNamespace}}",{{end}}
 	"message":"{{.API.Response.Message}}",
 	{{if .API.IsMulti}}
-	"result":{{if .API.Response.Result}}
-				{{$outputArray:=newArray}}
+	"result":{{isNil .API.Response.Result}}
+				null
+			 {{else}}
+			 	{{$outputArray:=newArray}}
 				{{range $apiName, $output := .API.Response.Result}}
 					{{$out:=printf "\"%s\":%s" $apiName $output}}
 					{{$outputArray:=$outputArray.Append $out}}
 				{{end}}
 				{{$outStr:=$outputArray.Join ","}}
 				{{printf "%s%s%s" "{" $outStr "}"}}
-			 {{else}}
-			 	null
 			 {{end}}
 	{{else}}
 		"result":{{if .API.Response.Result}}{{.API.Response.Result | getJSON}}{{else}}null{{end}}
